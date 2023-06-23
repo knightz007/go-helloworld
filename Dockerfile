2f992e032cc1c130789d4817f199a3f07f7110ef
anup.kumar2 <anup.kumar2@workday.com>
2023-06-23 23:38:13 +0530

    First check-in

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM golang:1.21rc2-alpine3.18
+EXPOSE 8080
---
+++ helloworld.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", HelloServer)
+	http.ListenAndServe(":8080", nil) // listening port
+}
+
+func HelloServer(rw http.ResponseWriter, req *http.Request) {
+	fmt.Fprint(rw, "Hello World!!")
+}
